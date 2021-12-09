2549b0cd2d76d3aaea3354e257760d71455286ed
Anonimo93 <70077670+Anonimo93@users.noreply.github.com>
2021-12-09 12:53:19 -0500

    Add files via upload

--- fcx_trader_test.py
+++ fcx_trader_test.py
@@ -43,7 +43,7 @@ def getMessage():
 
 # @server.route("/")
 # def pay():
-#     return "This is the fcx trading bot you can reach me @FCX_trading_bot on telegram"
+#     return "This is the BTT Trading Express you can reach me @FCX_trading_bot on telegram"
 
 @server.route('/dashboard')
 def dashboard():
@@ -109,7 +109,7 @@ Says: <b>{fcx_dp.dp_status_text}</b>
                 fcx_dp.user.account_balance = fcx_dp.user.account_balance + fcx_dp.recieved_amount
                 fcx_dp.commit()
                 text = f"You have been credited {fcx_dp.recieved_amount}"
-                dashboard[lang].keyboard[0][0] = f"Balances  {fcx_user.account_balance} BTC"
+                dashboard[lang].keyboard[0][0] = f"Balances  {fcx_user.account_balance} BTT"
                 bot.send_message(
                     fcx_dp.user.user_id,
                     text=text,
--- settings.py
+++ settings.py
@@ -1,27 +1,26 @@
 # Bot token provided by BotFather
 
-# TOKEN_TEST = "852053528:AAHL_ryUUJ1JOhenzmI0WDiayAnxxqGFmyU"
 from sqlalchemy import create_engine
 
 
-# PUBLIC_KEY = "953b0c668c9d75c2d3da984f62a00fd269dc66c6da701250a0d7e14b52449183"
-# PRIVATE_KEY = "c68f21F77B13FE4D6617EfcD0287c036da7A3aB1A5f3e870fb179E940F5839Dd"
-# Merchant_ID = "c4baf6ef23be73a2da7fa0531b2df323"
-# IPN_secret = "coinpaymentspeaks"
-# ADMIN_ID = 1053579181
+# PUBLIC_KEY = "80343df2dff487a2499ac6b2fc7d832d0b80cb4566107f6a03d4404e5b289b4e"
+# PRIVATE_KEY = "4672CFA54E54975177E5A2ae5a44e125c3687d9cdF15606Da96c2ba49B994037"
+# Merchant_ID = "7a52533349195fd518a34ffd446c8a0a"
+# IPN_secret = "btttradingexpress"
+# ADMIN_ID = 1355336561
 
 DEBUG = not True
 
 if DEBUG==False:
     print("\033[1;35;40m Running in production mode")
-    TOKEN = "1137661512:AAEig943WBK2aCBhlrDxgpN6Tl__lpxOMUY" #FCX trading bot
-    URL = 'https://fcx-trading.herokuapp.com/'
+    TOKEN = "5086018821:AAGjDVmpp-bMK4MbSxjmgBAAP9VNFcceO-E" #BTT Trading Express
+    URL = 'https://btt-trading-express.herokuapp.com/'
     try:
         import os
         DATABASE_URL = os.environ['DATABASE_URL']
     except KeyError:
-        # DATABASE_URL="postgres://oilzaezgbpfuad:0c38dcf0bdd1cad9456aff15f7b6ae3cb076e5911dcbb5bf266afd5a3710e608@ec2-184-72-236-57.compute-1.amazonaws.com:5432/d3u443uoa0b5os"
-        DATABASE_URL="postgres://jiuyexwrlknkee:183e46190d220e36b4401f4d6d86549ba7107653bb62ddadec6bccecd02c366a@ec2-54-246-89-234.eu-west-1.compute.amazonaws.com:5432/daprusoucv4h82"
+        # DATABASE_URL="postgres://ijmhoygudgmxru:49370293052dcaf5a977de733a4cb129f0f6a001f2a64da604fc4a4b5927af7e@ec2-44-193-111-218.compute-1.amazonaws.com:5432/d822275vn10alp"
+        DATABASE_URL="postgres://ijmhoygudgmxru:49370293052dcaf5a977de733a4cb129f0f6a001f2a64da604fc4a4b5927af7e@ec2-44-193-111-218.compute-1.amazonaws.com:5432/d822275vn10alp"
     db_url = DATABASE_URL.split(":")
 
     DATABASE_URI_VAR =f'postgres+psycopg2:{db_url[1]}:{db_url[2]}:{db_url[3]}'
@@ -30,9 +29,8 @@ if DEBUG==False:
 
 else:
     print("\033[1;32;40m Running in Development mode")
-    TOKEN = "852053528:AAHL_ryUUJ1JOhenzmI0WDiayAnxxqGFmyU"
+    TOKEN = "5086018821:AAGjDVmpp-bMK4MbSxjmgBAAP9VNFcceO-E"
 
-    # TOKEN = "746406709:AAHGsGOKxHwPOhRMdUOM5JNKsVxI2cCTbyQ" #fcxtrader bot
     URL = "https://3a7a746b.ngrok.io/"
     DATABASE_URL = 'postgres+psycopg2://postgres:postgres@localhost:5432'
     ADMIN_ID = 1053579181
